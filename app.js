e733ec7e7824223bc1ad29333c468fea071d6f5b
ConfirmedSk <flamemanaxis89@gmail.com>
2023-01-22 02:33:49 -0700

    Added date

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("GOODBYE WORLD");
+const date = new Date();
+
+const year = date.getFullYear();
+const day = date.getDate();
+const month = date.getMonth();
+
+function monthGetter(m) {
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	return months[m];
+}
+
+console.log(day);
+console.log(year);
+console.log(monthGetter(date.getMonth()));
+
+const node = document.getElementById("time");
+node.innerHTML = `${monthGetter(date.getMonth())} ${day}, ${year}`;
+document.getElementById("node").appendChild(node);
